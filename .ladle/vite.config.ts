81d7501a11d50e3272d24f934c60c475cc432ada
huydang-teq <huydang@teqnological.asia>
2023-09-26 16:50:38 +0700

    feat: add unoptimize useRouter

--- .ladle/vite.config.ts
+++ .ladle/vite.config.ts
@@ -12,6 +12,7 @@ export default ({ mode }) => {
       alias: {
         "next/image": path.resolve(__dirname, "./UnoptimizedImage.tsx"),
         "next/link": path.resolve(__dirname, "./UnoptimizedLink.tsx"),
+        "next/router":  path.resolve(__dirname, "./UnoptimizedUseRouter.tsx"),
       },
     },
   });
